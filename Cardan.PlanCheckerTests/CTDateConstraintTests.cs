3d235267fddc19707648569657f4fd372d9b5855
Rex Cardan <rexcardan@gmail.com>
2018-10-31 10:39:30 -0500

    added test project and test stub

---
+++ Cardan.PlanCheckerTests/CTDateConstraintTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cardan.PlanChecker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cardan.PlanChecker.Tests
+{
+    [TestClass()]
+    public class CTDateConstraintTests
+    {
+        [TestMethod()]
+        public void ConstrainTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
