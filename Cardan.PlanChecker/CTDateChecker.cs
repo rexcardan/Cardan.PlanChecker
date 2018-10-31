f21283dab632a55f1d67d3aeaf762d9200468c0e
Rex Cardan <rexcardan@gmail.com>
2018-10-31 10:34:50 -0500

    Added CT plan date constraint

---
+++ Cardan.PlanChecker/CTDateChecker.cs
@@ -0,0 +1,44 @@
+﻿using ESAPIX.Constraints;
+using ESAPIX.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMS.TPS.Common.Model.API;
+using F = ESAPIX.Facade.API;
+namespace Cardan.PlanChecker
+{
+    public class CTDateConstraint : IConstraint
+    {
+        public string Name => "CT Date Checker";
+        public string FullName => "CT Date < 60 Days";
+
+        public ConstraintResult CanConstrain(PlanningItem pi)
+        {
+            var pq = new PQAsserter(pi);
+            return pq.HasImage().CumulativeResult;
+        }
+
+        public ConstraintResult Constrain(PlanningItem pi)
+        {
+            var image = pi.GetImage();
+            return Constrain(new F.Image(image));
+        }
+
+        public ConstraintResult Constrain(F.Image image)
+        {
+            var diffDays = (DateTime.Now - image.CreationDateTime).Value.TotalDays;
+            var msg = $"CT is {diffDays} days old";
+
+            if (diffDays <= 60)
+            {
+                return new ConstraintResult(this, ResultType.PASSED, msg);
+            }
+            else
+            {
+                return new ConstraintResult(this, ResultType.ACTION_LEVEL_3, msg);
+            }
+        }
+    }
+}
--- Cardan.PlanChecker/ViewModels/MainViewModel.cs
+++ Cardan.PlanChecker/ViewModels/MainViewModel.cs
@@ -54,7 +54,7 @@ namespace Cardan.PlanChecker.ViewModels
                 new PlanConstraint(ConstraintBuilder.Build("Rectum", "V75Gy[%] <= 15")),
                 new PlanConstraint(ConstraintBuilder.Build("Rectum", "V65Gy[%] <= 35")),
                 new PlanConstraint(ConstraintBuilder.Build("Bladder", "V80Gy[%] <= 15")),
-                //new PlanConstraint(new CTDateConstraint())
+                new PlanConstraint(new CTDateConstraint())
             });
         }
 
