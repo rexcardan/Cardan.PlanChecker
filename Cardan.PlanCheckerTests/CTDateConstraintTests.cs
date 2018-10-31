26f5fe09b5a03dfb5214c344709a9b9cc9fc4992
Rex Cardan <rexcardan@gmail.com>
2018-10-31 10:46:42 -0500

    Filled out tests and passing status

--- Cardan.PlanCheckerTests/CTDateConstraintTests.cs
+++ Cardan.PlanCheckerTests/CTDateConstraintTests.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ESAPIX.Facade.API;
+using ESAPIX.Constraints;
 
 namespace Cardan.PlanChecker.Tests
 {
@@ -12,9 +14,23 @@ namespace Cardan.PlanChecker.Tests
     public class CTDateConstraintTests
     {
         [TestMethod()]
-        public void ConstrainTest()
+        public void ConstrainPassesTest()
         {
-            Assert.Fail();
+            var im =  new Image();
+            im.CreationDateTime = DateTime.Now.AddDays(-59); //59 days ago
+            var expected = ResultType.PASSED;
+            var actual = new CTDateConstraint().Constrain(im).ResultType;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ConstrainFailsTest()
+        {
+            var im = new Image();
+            im.CreationDateTime = DateTime.Now.AddDays(-61); //61 days ago
+            var expected = ResultType.ACTION_LEVEL_3;
+            var actual = new CTDateConstraint().Constrain(im).ResultType;
+            Assert.AreEqual(expected, actual);
         }
     }
 }
