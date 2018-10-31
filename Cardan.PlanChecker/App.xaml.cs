b7d55dcc9cd475c7f4735b12ca8cdcd4bd33d361
Rex Cardan <rexcardan@gmail.com>
2018-10-31 08:31:46 -0500

    Add project files.

---
+++ Cardan.PlanChecker/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using ESAPIX.Bootstrapper;
+using Cardan.PlanChecker.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using V = VMS.TPS.Common.Model.API;
+
+namespace Cardan.PlanChecker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            string[] args = e.Args;
+            base.OnStartup(e);
+            var bs = new AppBootstrapper<MainView>(() => { return V.Application.CreateApplication(); });
+            //You can use the following to load a context (for debugging purposes)
+            //args = ContextIO.ReadArgsFromFile(@"..\Desktop\context.txt");
+            //Might disable (uncomment) for plugin mode
+            bs.IsPatientSelectionEnabled = true;
+            bs.Run(args);
+        }
+    }
+}
---
+++ Cardan.PlanChecker/ViewModels/MainViewModel.cs
@@ -0,0 +1,71 @@
+﻿using ESAPIX.Interfaces;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESAPIX.Common;
+using VMS.TPS.Common.Model.API;
+
+namespace Cardan.PlanChecker.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        AppComThread VMS = AppComThread.Instance;
+
+        public MainViewModel()
+        {
+            //Example data bind
+            OnPlanChanged(VMS.GetValue(sc => sc.PlanSetup));
+            //Handle plan changes
+            VMS.Execute(sc =>
+            {
+                sc.PlanSetupChanged += OnPlanChanged;
+            });
+        }
+
+        public void OnPlanChanged(PlanSetup ps)
+        {
+            VMS.Execute(sc =>
+            {
+                Id = ps?.Id;
+                UID = ps?.UID;
+                IsDoseCalculated = ps?.Dose != null;
+                NBeams = ps?.Beams.Count();
+            });
+        }
+
+        private string id;
+
+        public string Id
+        {
+            get { return id; }
+            set { SetProperty(ref id, value); }
+        }
+
+        private string uid;
+
+        public string UID
+        {
+            get { return uid; }
+            set { SetProperty(ref uid, value); }
+        }
+
+        private int? nBeams;
+
+        public int? NBeams
+        {
+            get { return nBeams; }
+            set { SetProperty(ref nBeams, value); }
+        }
+
+        private bool isDoseCalculated;
+
+        public bool IsDoseCalculated
+        {
+            get { return isDoseCalculated; }
+            set { SetProperty(ref isDoseCalculated, value); }
+        }
+    }
+}
